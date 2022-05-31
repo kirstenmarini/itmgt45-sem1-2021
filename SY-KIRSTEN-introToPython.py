dbac6b5e6117fbffbb469492ab47ca709839e6de
kirstenmarini <62242755+kirstenmarini@users.noreply.github.com>
2022-06-01 06:08:35 +0800

    Add files via upload

---
+++ SY-KIRSTEN-introToPython.py
@@ -0,0 +1,15 @@
+'''
+#ID Number: 194760
+#Surname: SY
+#Year and Course: 3 BS ITE
+'''
+
+x = input('Input the first number:')
+y = input('Input the second number:')
+z = input('Input the third number:')
+
+sum = float(x) + float(y) + float(z)
+
+avg = sum/3
+
+print(avg)
